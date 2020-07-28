6474bbbda3e3833e525654d23fda22016954312b
JeanneBeula <nzabihijanebeula@gmail.com>
2020-07-28 17:11:44 +0200

    setUp new project

---
+++ src/database/config/config.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    host_name: process.env.HOST_NAME,
+    logging: false,
+  },
+};
